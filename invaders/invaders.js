013745cd2ae47cd6529e136b24a13ec3f53e0843
yacklebeam <jaketroxel@gmail.com>
2015-11-24 10:51:04 -0800

    gun, menu, and objectpool updates to invaders

--- invaders/invaders.js
+++ invaders/invaders.js
@@ -34,10 +34,12 @@
 
         init: function() {
             this.gameOver = false;
+            this.MenuOpen = false;
             Background.init();
             Hud.init();
-            Invaders.init();
-            Bullets.init();
+            Gun.init();
+            InvaderPool.init();
+            BulletPool.init();
             Ship.init();
         },
 
@@ -48,13 +50,17 @@
 
         draw: function() {
             if(!Game.gameOver) {
-                ctx.clearRect(0, 0, this.width, this.height);
-
-                Background.draw();
-                Hud.draw();
-                Invaders.draw();
-                Ship.draw();
-                Bullets.draw();
+                if(this.MenuOpen) {
+                    //Menu.draw();
+                    Hud.drawMenu();
+                } else {
+                    ctx.clearRect(0, 0, this.width, this.height);
+                    Background.draw();
+                    Hud.draw();
+                    //InvaderPool.draw();
+                    Ship.draw();
+                    BulletPool.draw();
+                }
             }
         },
 
@@ -67,6 +73,18 @@
             Game.canvas.removeEventListener('click', Game.runGame, false);
             Game.init();
             Game.animate();
+        }
+    };
+
+    var Menu = {
+        draw: function() {
+            ctx.fillStyle = '#091122';
+            ctx.fillRect(0, 0, Game.width, Game.height);
+
+            ctx.fillStyle = 'white';
+            ctx.textAlign = 'center';
+            ctx.font = '40px helvetica, arial';
+            ctx.fillText("PAUSED", Game.width / 2, Game.height / 2);
         }
     };
 
@@ -96,7 +114,8 @@
         },
 
         create : function() {
-            ctx.fillStyle = '#091122';
+            //ctx.fillStyle = '#091122';
+            ctx.fillStyle = 'black';
             ctx.fillRect(0, 0, Game.width, Game.height);
 
             ctx.fillStyle = this.textColor;
@@ -107,6 +126,93 @@
             ctx.fillStyle = '#999999';
             ctx.font = '20px helvetica, arial';
             ctx.fillText(this.textSub, Game.width / 2, Game.height / 2 + 30);
+        }
+    };
+
+    var Level = {
+        loadLevel: function(level) {
+            switch(level) {
+                case 1:
+                    Hud.goal = 100;
+                    for(var i = 0; i < 100; i++) {
+                        var newInvader = {
+                            life: 2,
+                            x: 200 + (i%10) * 25,
+                            y: 0 - 25 * Math.floor(i / 10),
+                            dead: false
+                        };
+                        InvaderPool.addNew(newInvader);
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+    };
+
+    var Gun = {
+        init: function() {
+            this.setGun(1);
+        },
+
+        setGun: function(id) {
+            switch(id) {
+                case 0:
+                    this.dmg = 1;
+                    this.r = 10;
+                    this.bulletCount = 1;
+                    this.bulletSpread = 1;
+                    this.fireRate = 10;
+                    this.color = 'red';
+                    break;
+                case 1:
+                    this.dmg = 2;
+                    this.r = 15;
+                    this.bulletCount = 9;
+                    this.bulletSpread = 3;
+                    this.fireRate = 5;
+                    this.color = 'red';
+                    break;
+                default:
+                    break;
+            }
+        },
+
+        getBulletDirection: function(bulletCount, i) {
+            if(i < (Math.floor(bulletCount / 2))) return (i - Math.floor(bulletCount / 2));
+            else return (i - Math.floor(bulletCount / 2) + 1);
+        },
+
+        shoot: function() {
+            if(this.bulletCount % 2 == 0) {
+                for(var i = 0; i < this.bulletCount; i++) {
+                    var newBullet = {
+                        x: Ship.x + 2 * this.getBulletDirection(this.bulletCount, i),
+                        y: Ship.y - 15,
+                        xs: this.bulletSpread * this.getBulletDirection(this.bulletCount, i),
+                        ys: -15,
+                        r: this.r,
+                        dmg: this.dmg,
+                        color: this.color
+                    };
+                    BulletPool.addNew(newBullet);
+                }
+            } else {
+                for(var i = 0; i < this.bulletCount; i++) {
+                    var newBullet = {
+                        x: Ship.x + 2 * (i - (Math.floor(this.bulletCount / 2))),
+                        y: Ship.y - 15,
+                        xs: this.bulletSpread * (i - (Math.floor(this.bulletCount / 2))),
+                        ys: -15,
+                        r: this.r,
+                        dmg: this.dmg,
+                        color: this.color
+                    };
+                    BulletPool.addNew(newBullet);
+                }
+            }
         }
     };
 
@@ -137,6 +243,7 @@
         point4 : {x: 0, y: 0},
         
         init: function() {
+            this.resetTimer = Gun.fireRate;
             this.x = 290;
             this.y = 590;
             this.speed = 4;
@@ -162,175 +269,228 @@
         },
 
         move: function() {
-            if (Ctrl.left && this.x < 500) {
-                this.x += this.speed;
-            } else if (Ctrl.right && this.x > 100) {
+            if (Ctrl.left && this.x > 100) {
                 this.x += -this.speed;
+            } else if (Ctrl.right && this.x < 500) {
+                this.x += this.speed;
             }
 
             if(this.x > 700) this.x = 500;
             if(this.x < 100) this.x = 100;
 
             if(Ctrl.fire) {
-                Bullets.addBullet();
-                Ctrl.fire = false;
-                Ctrl.fireReset = true;
+                if(this.resetTimer == 0) {
+                    this.shoot();
+                    this.resetTimer = Gun.fireRate;
+                }
+                this.resetTimer--;
             }
+        },
+
+        shoot: function() {
+            Gun.shoot();
+            //Ctrl.fire = false;
+            //Ctrl.fireReset = true;
         }
 
     };
 
-    var Bullets = {
-        count : 100,
-        current : 0,
-
-        init : function() {
-            this.ents = [this.count];
+    var BulletPool = {
+        init: function() {
+            this.maxSize = 100;
+            this.count = 0;
+            this.objects = [];
+            this.open = 0;
+            this.end = 0;
         },
 
-        draw : function() {
+        draw: function() {
             if(Game.gameOver) return;
-            var i;
-            for(i = 0; i < this.count; i++) {
-                var B = this.ents[i];
-
-                if(B) {
-
-                    B.x += B.xs;
-                    B.y += B.ys;
-
-                    this.collide(i);
-                
+            for(var i = 0; i < this.end; i++) {
+                var bullet = this.objects[i];
+                if(bullet) {
+                    bullet.x += bullet.xs;
+                    bullet.y += bullet.ys;
+            
                     ctx.beginPath();
-                    ctx.arc(B.x, B.y, B.r, 0, 2 * Math.PI);
+                    ctx.arc(bullet.x, bullet.y, bullet.r, 0, 2 * Math.PI);
                     ctx.closePath();
 
-                    ctx.fillStyle = '#eee';
+                    ctx.fillStyle = bullet.color;
                     ctx.fill();
 
-                    if(B.y < -5) {
-                        this.ents[i] = null;
-                        this.current--;
-                    }
+                    ctx.beginPath();
+                    ctx.arc(bullet.x, bullet.y, bullet.r - 4, 0, 2 * Math.PI);
+                    ctx.closePath();
 
-                    if(Invaders.current == 0) {
-                        Game.gameOver = true;
-                        Screen.gamewin();
-                        Game.canvas.addEventListener('click', Game.restartGame, false);
+                    ctx.fillStyle = 'white';
+                    ctx.fill();
+
+                    if(bullet.y < -5) {
+                        this.removeIndex(bullet.index);
                     }
+                }
+            }
+
+            this.collide();
+        },
+
+        addNew: function(ob) {
+            if(this.open == this.end) {
+                ob.index = this.open;
+                this.objects.push(ob);
+                this.end++;
+                this.open++;
+            } else {
+                ob.index = this.open;
+                this.objects[this.open] = ob;
+                this.open = this.getOpenIndex();
+            }
+        },
+
+        removeIndex: function(index) {
+            this.objects[index] = null;
+            if(index < this.open) this.open = index;
+        },
+
+        collide: function() {
+            for(var i = 0; i < this.end; i++) {
+                var Bullet = this.objects[i];
+                if(Bullet) {
+                    for(var j = 0; j < InvaderPool.end; j++) {
+                        var Invader = InvaderPool.objects[j];
+                        if(Invader && !Invader.dead) {
+                            if(Math.sqrt((Invader.x - Bullet.x) * (Invader.x - Bullet.x) + (Invader.y - Bullet.y) * (Invader.y - Bullet.y)) < 15) {
+                                this.removeIndex(Bullet.index);
+                                InvaderPool.objects[j].life -= Bullet.dmg;
+                                if(InvaderPool.objects[j].life <= 0) {
+                                    //InvaderPool.removeIndex(Invader.index);
+                                    InvaderPool.objects[j].dead = true;
+                                    Hud.goal--;
+                                    Hud.score++;
+                                }
+                               /*if(Hud.goal == 0) {
+                                    Game.gameOver = true;
+                                    Screen.gamewin();
+                                    Game.canvas.addEventListener('click', Game.restartGame, false);
+                                    return;
+                                }*/
+                                break;
+                            }
+                        }
+                    }  
                 }
             }
         },
 
-        collide : function(index) {
-            var B = this.ents[index];
-            var i;
-            for(i = 0; i < Invaders.count; i++) {
-                var A = Invaders.ents[i];
-                if( A &&
-                    Math.sqrt(  (B.x - A.x) * (B.x - A.x) +
-                                (B.y - A.y) * (B.y - A.y)) < 13) { //hitting asteroid
-                    this.ents[index] = null;
-                    this.current--;
-                    Invaders.ents[i] = null;
-                    Invaders.current--;
-                    Hud.score++;
-
-                    break;
-                }
+        getOpenIndex: function() {
+            for(var i = 0; i < this.end; i++) {
+                if(this.objects[i] == null) return i;
             }
-        },
-
-        addBullet : function() {
-            if(this.current === this.count) return;
-            else {
-                var open;
-                var i;
-                for(i = 0; i < this.count; i++)
-                {
-                    if(this.ents[i] == null)
-                    {
-                        open = i;
-                        break;
-                    };
-                }
-                this.ents[open] = {
-                    x : Ship.x,
-                    y : Ship.y - 25,
-                    r : 3,
-                    xs : 0,
-                    ys : -10
-                };
-                this.current++;
-            }
+            return this.end;
         }
-
     };
 
-    var Invaders = {
-        count : 30,
-
-        getRandomInt : function(min, max) {
-            return Math.floor(Math.random() * (max - min)) + min;
+     var InvaderPool = {
+        init: function() {
+            this.maxSize = 100;
+            this.count = 0;
+            this.objects = [];
+            this.open = 0;
+            this.end = 0;
+            Level.loadLevel(1);
         },
 
-        init : function() {
-            this.current = this.count;
-            this.ents = [this.count];
-            var i;
-            for(i = 0; i < this.count; i++) {
-                var X = this.getRandomInt(1, 12);
-
-                var XIndex = 120 + X * 30;
-                var YIndex = -(i * 30);
-
-                this.addInvader(i, XIndex, YIndex, 2);
-            }
-        },
-
-        draw : function() {
+        draw: function() {
             if(Game.gameOver) return;
-            var i;
-            for(i = 0; i < this.count; i++) {
-                var A = this.ents[i];
+            for(var i = 0; i < this.end; i++) {
+                var A = this.objects[i];
 
                 if(A) {
+                    if(A.dead) {
+                        A.y += 5;
 
-                    A.y += 1;
+                        ctx.beginPath();
+                        ctx.arc(A.x, A.y, 4, 0, 2 * Math.PI);
+                        ctx.closePath();
+
+                        ctx.fillStyle = 'gold';
+                        ctx.fill(); 
+                    } else {
+                        A.y += 1;
                 
-                    ctx.beginPath();
-                    ctx.moveTo(A.x, A.y + 15);
-                    ctx.lineTo(A.x - 10, A.y - 10);
-                    ctx.lineTo(A.x + 10, A.y - 10);
-                    ctx.closePath();
-                    ctx.fillStyle = '#eee';
-                    ctx.fill();
+                        ctx.beginPath();
+                        ctx.moveTo(A.x, A.y + 15);
+                        ctx.lineTo(A.x - 10, A.y - 10);
+                        ctx.lineTo(A.x + 10, A.y - 10);
+                        ctx.closePath();
+                        if(A.life == 1) ctx.fillStyle = '#eee';
+                        if(A.life == 2) ctx.fillStyle = 'blue';
+                        ctx.fill();
+                    }
 
-                    if(A.y > Game.height - 30) {
+                    /*if(A.y > Game.height - 30) {
                         Game.gameOver = true;
                         Screen.gamelose();
                         Game.canvas.addEventListener('click', Game.restartGame, false);
                         return;
+                    }*/
+                    if(A.y == Game.height + 5) {
+                        this.removeIndex(A.index);
                     }
                 }
             }
         },
 
-        addInvader : function(i, xIn, yIn, lifeIn) {
-            var open = i;
-            
-            this.ents[open] = {
-                x : xIn,
-                y : yIn,
-                life: lifeIn
-            };
+        addNew: function(ob) {
+            if(this.open == this.end) {
+                ob.index = this.open;
+                this.objects.push(ob);
+                this.end++;
+                this.open++;
+            } else {
+                ob.index = this.open;
+                this.objects[this.open] = ob;
+                this.open = this.getOpenIndex();
+            }
+        },
+
+        removeIndex: function(index) {
+            this.objects[index] = null;
+            if(index < this.open) this.open = index;
+        },
+
+        getOpenIndex: function() {
+            for(var i = 0; i < this.end; i++) {
+                if(this.objects[i] == null) return i;
+            }
+            return this.end;
         }
     };
 
     var Hud = {
         init: function() {
-            this.lv = 0;
+            this.mainMenu = [
+            {
+                text: "GUN 1",
+                cmd: function() {
+                    Gun.setGun(0);
+                    Game.MenuOpen = false;
+                }
+            },
+            {
+                text: "GUN 2",
+                cmd: function() {
+                    Gun.setGun(1);
+                    Game.MenuOpen = false;
+                }
+            }];
+            this.level = 0;
             this.score = 0;
+            this.menux = 200;
+            this.menuy = 200;
+            this.gunLevel = 0;
+            this.selected = 0;
         },
 
         draw: function() {
@@ -339,8 +499,33 @@
             ctx.fillStyle = 'white';
             ctx.textAlign = 'left';
             ctx.fillText('Score: ' + this.score, 5, Game.height - 5);
-            ctx.textAlign = 'right';
-            ctx.fillText('Level: ' + this.lv, Game.width - 5, Game.height - 5);
+            ctx.textAlign = 'left';
+            ctx.fillText('Level: ' + this.level, 5, Game.height - 19);
+        },
+
+        drawMenu: function() {
+            if(Ctrl.left && Ctrl.leftReset && this.selected > 0) {
+                this.selected--;
+                Ctrl.leftReset = false;
+            }
+            else if(Ctrl.right && Ctrl.rightReset && this.selected < this.mainMenu.length - 1) {
+                this.selected++; 
+                Ctrl.rightReset = false;
+            }
+            ctx.clearRect(this.menux, this.menuy, 200, 200);
+            ctx.fillStyle = 'black';
+            ctx.fillRect(this.menux, this.menuy, 200, 200);
+            ctx.font = '12px helvetica, arial';
+            ctx.fillStyle = 'white';
+            ctx.textAlign = 'left';
+            for(var i = 0; i < this.mainMenu.length; i++) {
+                var text = this.mainMenu[i].text;
+                if(i == this.selected) text += " <";
+                ctx.fillText(text, this.menux + 5, this.menuy + 17 + (i * 14));
+            }
+            if(Ctrl.fire) {
+                this.mainMenu[this.selected].cmd();
+            }
         }
     };
 
@@ -353,15 +538,27 @@
 
         keyDown: function(event) {
             switch(event.keyCode) {
-                case 39: // Left
+                case 65: //A
                     Ctrl.left = true;
                     break;
-                case 37: // Right
+                case 68: //D
                     Ctrl.right = true;
                     break;
-                case 87: //Space
-                    if(Ctrl.fireReset == false) Ctrl.fire = true;
+                case 16: //Shift or
+                case 74: //J
+                    Ctrl.fire = true;
+                    if(Ctrl.fireReset == false) {
+                        Ship.resetTimer = 0;
+                        Ctrl.fireReset = true;
+                    }
                     break;
+                case 80: //P
+                    Game.MenuOpen = !Game.MenuOpen;
+                    break;
+                /*case 85:
+                    Hud.gunLevel++;
+                    Gun.setGun(Hud.gunLevel);
+                    break;*/
                 default:
                     break;
             }
@@ -369,14 +566,18 @@
 
         keyUp: function(event) {
             switch(event.keyCode) {
-                case 39: // Left
+                case 65: //A
                     Ctrl.left = false;
+                    Ctrl.leftReset = true;
                     break;
-                case 37: // Right
+                case 68: //D
                     Ctrl.right = false;
+                    Ctrl.rightReset = true;
                     break;
-                case 87: //Space
+                case 16: //Shift or
+                case 74: //J
                     Ctrl.fireReset = false;
+                    Ctrl.fire = false;
                     break;
                 default:
                     break;
